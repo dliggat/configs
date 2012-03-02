64144cdc47b7f92632c0db6072247827fed4b2a5
Dave L <liggsie@gmail.com>
2012-03-02 10:53:34 -0800

    Forgot to change echo to cp

--- copy_to.sh
+++ copy_to.sh
@@ -17,17 +17,17 @@ GIT_DIR="/Users/$USER/git/configs/os_x"
 if [ $1 == 'to_local' ]
 then
   echo "Copying files from git repository to local..."
-  echo "$GIT_DIR/bash_profile.sh"  "$LOCAL_DIR/.bash_profile"
-  echo "$GIT_DIR/bashrc.sh"        "$LOCAL_DIR/.bashrc"
-  echo "$GIT_DIR/vimrc"            "$LOCAL_DIR/.vimrc"
-  echo "$GIT_DIR/gvimrc"           "$LOCAL_DIR/.gvimrc"
+  cp "$GIT_DIR/bash_profile.sh"  "$LOCAL_DIR/.bash_profile"
+  cp "$GIT_DIR/bashrc.sh"        "$LOCAL_DIR/.bashrc"
+  cp "$GIT_DIR/vimrc"            "$LOCAL_DIR/.vimrc"
+  cp "$GIT_DIR/gvimrc"           "$LOCAL_DIR/.gvimrc"
 elif [ $1 == 'to_git' ]
 then
   echo "Copying files from local to git repository..."
-  echo "$LOCAL_DIR/.bash_profile"  "$GIT_DIR/bash_profile.sh"  
-  echo "$LOCAL_DIR/.bashrc"        "$GIT_DIR/bashrc.sh"        
-  echo "$LOCAL_DIR/.vimrc"         "$GIT_DIR/vimrc"            
-  echo "$LOCAL_DIR/.gvimrc"        "$GIT_DIR/gvimrc"           
+  cp "$LOCAL_DIR/.bash_profile"  "$GIT_DIR/bash_profile.sh"  
+  cp "$LOCAL_DIR/.bashrc"        "$GIT_DIR/bashrc.sh"        
+  cp "$LOCAL_DIR/.vimrc"         "$GIT_DIR/vimrc"            
+  cp "$LOCAL_DIR/.gvimrc"        "$GIT_DIR/gvimrc"           
 else
   echo "Error. $(usage)"
   exit 1
