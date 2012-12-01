8caeaf88a44ce1e26c925fe1fda54c39be55d6ec
Dave Liggat <liggsie@gmail.com>
2012-12-01 17:33:01 -0500

    Moving heroku and rvm stuff to bashrc

--- os_x/bashrc.sh
+++ os_x/bashrc.sh
@@ -48,6 +48,14 @@ function shortpath {
   echo $newPWD
 }
 
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
+
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+    . `brew --prefix`/etc/bash_completion
+fi
 
 # Taken from http://www.cyberciti.biz/faq/bash-shell-change-the-color-of-my-shell-prompt-under-linux-or-unix/
 # Also read this: http://superuser.com/questions/246625/bash-command-prompt-overwrites-the-current-line
